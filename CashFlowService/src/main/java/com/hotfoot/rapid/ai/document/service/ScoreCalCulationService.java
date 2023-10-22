fe89f0f3cbfc93697021f341f3ef107bfc887f4d
prithivi.rs <prithivi.rs@hotfoot.co.in>
2023-10-22 10:11:16 +0200

    new project

---
+++ CashFlowService/src/main/java/com/hotfoot/rapid/ai/document/service/ScoreCalCulationService.java
@@ -0,0 +1,15 @@
+package com.hotfoot.rapid.ai.document.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScoreCalCulationService {
+
+	public String getScore(String customerId) {
+		
+		return "score is 5,1";
+	}
+	
+	
+
+}
