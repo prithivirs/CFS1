ef546c040b2a9972a50a3417c4091d8f0cdd32fc
Sandeep <sandeep.s@hotfoot.co.in>
2023-10-29 08:15:09 +0100

    analysis api added

--- cash-flow-service/src/main/java/com/hotfoot/rapid/ai/document/controller/BankStatementController.java
+++ cash-flow-service/src/main/java/com/hotfoot/rapid/ai/document/controller/BankStatementController.java
@@ -1,16 +1,30 @@
 package com.hotfoot.rapid.ai.document.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hotfoot.rapid.ai.document.service.BankStatementService;
 
 @Component
 @RestController
 public class BankStatementController {
 	
+	@Autowired
+	private BankStatementService bankStatementService;
 
 	@GetMapping(value="rest/bank/statement/upload")
 	private String UploadBankStatement() {
 		return null;
 	}
+	
+	@GetMapping(value="rest/analyze/pdf")
+	private ResponseEntity<String> analyzePdf(@RequestParam("pdf_name")String pdfName) {
+		return bankStatementService.validatePdf(pdfName);
+	}
+	
 }
---
+++ cash-flow-service/src/main/java/com/hotfoot/rapid/ai/document/service/BankStatementService.java
@@ -0,0 +1,35 @@
+package com.hotfoot.rapid.ai.document.service;
+
+import java.io.File;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BankStatementService {
+
+	public ResponseEntity<String> validatePdf(String pdfName) {
+		try {
+			File pdfFile = new File("/home/vagrant/images/" + pdfName);
+			PDDocument document = PDDocument.load(pdfFile);
+			PDFTextStripper pdfTextStripper = new PDFTextStripper();
+			// Get text content from the PDF
+			String pdfText = pdfTextStripper.getText(document);
+			if (pdfText.trim().length() < 30) {
+				document.close();
+				return ResponseEntity.status(HttpStatus.OK)
+						.body("PDF is Scanned and might take longer time to analyze");
+			} else {
+				document.close();
+				return ResponseEntity.status(HttpStatus.OK).body("PDF analysis in progress");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unable to identify authenticity of pdf");
+	}
+
+}
