c0b48859fd8b3f83cd7c16da52026cac25d27706
Sandeep <sandeep.s@hotfoot.co.in>
2023-12-30 10:38:03 +0100

    implementing white listed domains

--- cash-flow-service/src/main/java/com/hotfoot/rapid/ai/security/filter/TokenAuthenticationFilter.java
+++ cash-flow-service/src/main/java/com/hotfoot/rapid/ai/security/filter/TokenAuthenticationFilter.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import com.hotfoot.rapid.ai.security.datamodel.TokenCache;
@@ -22,43 +24,55 @@ import java.util.Date;
 
 @Component
 public class TokenAuthenticationFilter implements Filter {
-	
+
 	@Autowired
 	private TokenCreationService tokenService;
-	
+
 	@Autowired
 	private TokenCacheRepository tokenCacheRepo;
-	
+
 	@Autowired
 	private LogoutService logoutService;
+	
+	@Value("#{'${whitelisted.domains:localhost}'.split(',')}")
+	private String[] whiteListedDomains;
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // Initialization code, if needed
-    }
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// Initialization code, if needed
+	}
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        try {
-        	String tokenFromRequest = tokenService.getToken((HttpServletRequest) request);
-        	if(tokenFromRequest == null) {
-        		logoutService.logout((HttpServletRequest) request, (HttpServletResponse) response);
-        	}
-        	TokenCache activeToken = tokenCacheRepo.findByToken(tokenFromRequest);
-        	if(activeToken != null) {
-        		activeToken.setModifiedDate(new Date());
-        		tokenCacheRepo.save(activeToken);
-        	}
-            // Call the next filter in the chain (or the servlet if this is the last filter)
-            chain.doFilter(request, response);
-        } catch (Exception e) {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		try {
+			String referrer = ((HttpServletRequest) request).getHeader(HttpHeaders.REFERER);
+			boolean isValidReferrer = true;
+			for (String domainName : whiteListedDomains) {
+				if (!referrer.startsWith(domainName.trim())) {
+					isValidReferrer = false;
+					continue;
+				}
+			}
+			String tokenFromRequest = tokenService.getToken((HttpServletRequest) request);
+			if (tokenFromRequest == null || referrer == null || !isValidReferrer) {
+				logoutService.logout((HttpServletRequest) request, (HttpServletResponse) response);
+			} else {
+				TokenCache activeToken = tokenCacheRepo.findByToken(tokenFromRequest);
+				if (activeToken != null) {
+					activeToken.setModifiedDate(new Date());
+					tokenCacheRepo.save(activeToken);
+				}
+			}
+			// Call the next filter in the chain (or the servlet if this is the last filter)
+		} catch (Exception e) {
 			System.out.println("unable to update token's validity");
 		}
-    }
+		chain.doFilter(request, response);
+	}
 
-    @Override
-    public void destroy() {
-        // Cleanup code, if needed
-    }
+	@Override
+	public void destroy() {
+		// Cleanup code, if needed
+	}
 }
