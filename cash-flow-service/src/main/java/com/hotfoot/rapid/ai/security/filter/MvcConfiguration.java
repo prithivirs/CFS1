a764bb1aa41eeb71c845e01ebaa126abcf5b9c3b
Sandeep <sandeep.s@hotfoot.co.in>
2023-12-31 12:05:36 +0100

    adding cors

--- cash-flow-service/src/main/java/com/hotfoot/rapid/ai/security/filter/MvcConfiguration.java
+++ cash-flow-service/src/main/java/com/hotfoot/rapid/ai/security/filter/MvcConfiguration.java
@@ -1,14 +1,25 @@
 package com.hotfoot.rapid.ai.security.filter;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfiguration extends WebMvcConfigurerAdapter {
 	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("*");
+	@Bean
+	public CorsFilter corsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.addAllowedOrigin("*");
+	    config.addAllowedMethod("*");
+	    config.addAllowedHeader("*");
+	    source.registerCorsConfiguration("/**", config);
+	    return new CorsFilter(source);
 	}
+
+	
 }
